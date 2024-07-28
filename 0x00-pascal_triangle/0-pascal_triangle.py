f8d3adab61bf768a418137968036bee2ac2f156f
Mukeli3 <mukelicynthia20@gmail.com>
2024-07-28 19:01:26 +0000

    Py Pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    """
+    function returns a list of lists of integers representing the Pascal’s triangle of n
+    """
+    if n <= 0:
+        return []
+
+    """ initialize the first triangle row """
+    tri = [[1]]
+
+    for i in range(1, n):
+        """ start each row with 1 """
+        row = [1]
+        for j in range(1, i):
+            """ each row is the sum of the two elements above it """
+            row.append(tri[i - 1][j - 1] + tri[i - 1][j])
+
+        """ end each row with 1 """
+        row.append(1)
+        tri.append(row)
+
+    return tri
