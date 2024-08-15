5be983760890f83b585a1b2f9cbf05ae85e91797
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-15 23:45:43 +0000

    pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -13,6 +13,7 @@ def print_stats(fsize, counts) -> None:
         if counts[code] > 0:
             print(f"{code}: {counts[code]}")
 
+
 total_fsize = 0
 counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 lcount = 0
