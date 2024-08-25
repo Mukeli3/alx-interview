08ea6ac2c2a02ebdb4b482a0e7b72345ddadf243
Mukeli3 <mukelicynthia20@gmail.com>
2024-08-25 22:07:33 +0000

    validate utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+This module defines a method that determines if a given data set represents a
+valid UTF-8 encoding
+"""
+
+def validUTF8(data):
+    """
+    Method determines if a given data set represents a valid UTF-8 encoding
+
+    Args:
+        data (int list): A list of ints, each reps 1 byte
+
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else false
+    """
+    n = 0 # current UTF-8 character bytes number
+
+    # masks, check leading buts
+    f_mask = 1 << 7
+    s_mask = 1 << 6
+
+    for num in data:
+        byte = num & 0xFF
+        if n == 0:
+            if (byte & f_mask) == 0: # determine UTF-8 character bytes number
+                continue # 1-byte character
+            elif (byte & (f_mask >> 1)) == f_mask: # invalid byte
+                return False
+            elif (byte & (f_mask >> 2)) == (f_mask >> 1):
+                n = 1 # 2-byte character
+            elif (byte & (f_mask >> 3)) == (f_mask >> 2):
+                n = 2 # 3-byte character
+            elif (byte & (f_mask >> 4)) == (f_mask >> 3):
+                n = 3
+            else:
+                if not (byte & f_mask and not (byte & s_mask)):
+                    return False
+                n -= 1
+    return n == 0
