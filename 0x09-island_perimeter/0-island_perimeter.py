97d6670811aa80d69422dffa9ce191285e5a146d
Mukeli3 <mukelicynthia20@gmail.com>
2024-09-26 14:08:06 +0000

    documentation and pycodestyle fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -7,19 +7,30 @@ perimeter.
 
 def island_perimeter(grid):
     """
+    Function returns the island perimeter
+    Args:
+        grid - list of list of ints
+             0 reps water
+             1 reps land
+             Each call is square, with 1 side length
+             calls are connected horizontally/vertically (not diagonally)
+             is rectangular, width and height not exceeding 100
+             completely surrounded by water
+    Returns:
+          the island perimeter
     """
-    perimeter = 0 # track island total perimeter
+    perimeter = 0  # track island total perimeter
 
     row = len(grid)
-    col = len(grid[0]) # grid first row length
+    col = len(grid[0])  # grid first row length
 
     for i in range(row):
         for j in range(col):
-            if grid[i][j] == 1: # current cell is 1, land
+            if grid[i][j] == 1:  # current cell is 1, land
                 perimeter += 4
                 # check for land cell directly above the currant cell
-                if i > 0 and grid[i -  1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1: # to the left
+                if j > 0 and grid[i][j - 1] == 1:  # to the left
                     perimeter -= 2
     return perimeter
